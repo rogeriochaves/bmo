829758f31a31ff6d688772ddd2fa68c5d68c93ea
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2023-05-14 23:26:07 +0200

    Implement reply on silence mode when assistant is woken up, allowing you to keep a conversation with it

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Any, Literal
 from lib.delta_logging import logging, red, reset  # has to be the first import
 from dotenv import load_dotenv  # has to be the second
 
@@ -12,6 +13,7 @@ import pvporcupine
 from pvrecorder import PvRecorder
 from io import BytesIO
 import openai
+import numpy as np
 
 picovoice_access_key = os.environ["PICOVOICE_ACCESS_KEY"]
 openai.api_key = os.environ["OPENAI_API_KEY"]
@@ -24,6 +26,13 @@ porcupine = pvporcupine.create(
 )
 frame_length = porcupine.frame_length  # 512
 buffer_size = frame_length * 32 * 5  # keeps 5s of audio
+sample_rate = 16000  # sample rate for Porcupine is fixed at 16kHz
+silence_threshold = 300  # maybe need to be adjusted
+silence_limit = 2 * sample_rate // frame_length  # 2 seconds of silence
+speaking_minimum = 0.5 * sample_rate // frame_length  # 0.5 seconds of speaking
+silence_time_to_standby = (
+    10 * sample_rate // frame_length
+)  # goes back to wakeup word checking after 10s of silence
 
 
 def create_audio_file(audio_buffer):
@@ -41,47 +50,88 @@ def create_audio_file(audio_buffer):
     return virtual_file
 
 
-def main():
-    recorder = PvRecorder(device_index=-1, frame_length=porcupine.frame_length)
-    recorder.start()
+ListeningMode = Literal["waiting_for_wakeup", "reply_on_silence"]
+
+
+def conversation_loop(recorder: PvRecorder):
+    silence_frame_count = 0
+    speaking_frame_count = 0
+    listening_mode: ListeningMode = "waiting_for_wakeup"
 
     logger.info("Listening ... (press Ctrl+C to exit)")
 
     audio_buffer = bytearray()
+
+    while True:
+        pcm = recorder.read()
+        print(f"🔴 {red}Recording...{reset} silence_frame_count: {silence_frame_count} speaking_frame_count: {speaking_frame_count}", end="\r", flush=True)
+
+        trigger = -1
+        if listening_mode == "waiting_for_wakeup":
+            trigger = porcupine.process(pcm)
+            if trigger >= 0:
+                logger.info("Detected wakeup word #%s", trigger)
+        elif listening_mode == "reply_on_silence":
+            rms = np.sqrt(np.mean(np.array(pcm) ** 2))
+            if rms < silence_threshold:
+                silence_frame_count += 1
+            else:
+                if speaking_frame_count >= speaking_minimum:
+                    silence_frame_count = 0
+                speaking_frame_count += 1
+
+            if (
+                silence_frame_count >= silence_limit
+                and speaking_frame_count >= speaking_minimum
+            ):
+                logger.info("Detected silence a while after speaking, giving a reply")
+                trigger = 0
+
+            if silence_frame_count >= silence_time_to_standby:
+                logger.info(
+                    "Long silence time, going back to waiting for the wakeup word"
+                )
+                silence_frame_count = 0
+                speaking_frame_count = 0
+                listening_mode = "waiting_for_wakeup"
+
+        audio_buffer.extend(struct.pack("h" * len(pcm), *pcm))
+        if len(audio_buffer) > buffer_size:
+            audio_buffer = audio_buffer[
+                frame_length:
+            ]  # drop early frames to keep just most recent audio
+
+        if trigger >= 0:
+            elevenlabs.play_audio_file_non_blocking("beep.mp3")
+            audio_file = create_audio_file(audio_buffer)
+            logger.info("Built wav file")
+
+            recorder.stop()
+            transcription: Any = openai.Audio.transcribe("whisper-1", audio_file)
+            logger.info("Transcription: %s", transcription["text"])
+
+            reply = chatgpt.reply(transcription["text"])
+            elevenlabs.play_audio_file_non_blocking("beep2.mp3")
+            logger.info("Reply: %s", reply["content"])
+
+            audio_stream = elevenlabs.text_to_speech(reply["content"])
+
+            elevenlabs.play(audio_stream)
+            logger.info("Playing audio done")
+
+            listening_mode = "reply_on_silence"
+            silence_frame_count = 0
+            speaking_frame_count = 0
+            audio_buffer = bytearray()
+
+            recorder.start()
+
+
+def main():
+    recorder = PvRecorder(device_index=-1, frame_length=porcupine.frame_length)
+    recorder.start()
     try:
-        while True:
-            pcm = recorder.read()
-            result = porcupine.process(pcm)
-
-            print(f"🔴 {red}Recording...{reset}", end="\r", flush=True)
-
-            audio_buffer.extend(struct.pack("h" * len(pcm), *pcm))
-            if len(audio_buffer) > buffer_size:
-                audio_buffer = audio_buffer[
-                    frame_length:
-                ]  # drop early frames to keep just most recent audio
-
-            if result >= 0:
-                logger.info("Detected hotword #%s", result)
-                elevenlabs.play_audio_file_non_blocking("beep.mp3")
-                audio_file = create_audio_file(audio_buffer)
-                logger.info("Built wav file")
-
-                recorder.stop()
-                transcription = openai.Audio.transcribe("whisper-1", audio_file)
-                logger.info("Transcription: %s", transcription["text"])
-
-                reply = chatgpt.reply(transcription["text"])
-                elevenlabs.play_audio_file_non_blocking("beep2.mp3")
-                logger.info("Reply: %s", reply["content"])
-
-                audio_stream = elevenlabs.text_to_speech(reply["content"])
-
-                elevenlabs.play(audio_stream)
-                logger.info("Playing audio done")
-
-                recorder.start()
-
+        conversation_loop(recorder)
     except KeyboardInterrupt:
         print("Stopping ...")
     finally:
