2a0d85a9c654a4b5752a03a5ef978de26e6b7e2c
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2023-05-20 08:53:38 +0200

    Remove completely the need for audio similarity! Volume is all you need

--- lib/interruption_detection.py
+++ lib/interruption_detection.py
@@ -1,35 +1,20 @@
 from multiprocessing import Process, Queue
-import os
 from queue import Empty
-from python_speech_features import mfcc
-from scipy.spatial.distance import cosine
 from typing import Any, List, Optional
 import numpy as np
-import math
 
 from lib.utils import terminate_pid_safely, calculate_volume
 
-# This code is quite convoluted, trying to do a hacky, but lightweight, way of detecting interruption on the fly.
-# It is necessary because we cannot do a simple volume check, since there might be an echo loop when the speakers
-# are too close from the microphone, and all previous attempts of echo cancellation I've tried, failed, it's very hard.
+# This code is a lightweight way of detecting interruption on the fly.
 #
-# The way this works is by getting both the pcm, that is the pcm data incoming from user's microphone,
-# and the reply_audio, that is, the audio chunks produced by ElevenLabs. We capture the first 16 * 5 chunks of PCMs
-# from both input and output, and we analyse their similarity using a very simple method of cosine similarity.
-# If it's more than 96% similar (silence already achieves 94% similarity, so we need to be stricter), then we consider
-# there is a feedback loop from the microphone
+# The way it works is by trying to match when the audio from the assistant reply started playing, and measure the
+# average volume on the first 16 * 5 chunks of PCMs, if there is a feedback loop from speaker to mic, the average volume
+# will be higher than if there is silence
 #
-# Then, we behave in two different ways: if we decide there is no echo feedback loop, then it's easy, we try to detect
-# a volume louder than the silence threshold, if that's the case, we interrupt the assistant audio. But in the case of
-# having a feedback loop, looking at similarity only didn't work well enough, so we look at volume differences, that is
-# if you speak during the silence that assistant has in between sentences, then we can detect a diff in volumes and use
-# that as interruption
+# So, at every frame, we detect if the mic volume suddenly got louder than the initial 5 batch frames average, if so, we
+# interrupt the assistant
 #
-# For the first chunks similarity detection to work well, both the pcm and the reply_audio must be very aligned,
-# but trial-and-error, I reached the guess number of 28 frames to skip after ffplay is started. Then, for the volume diff
-# of the echo feedback loop scenario, we need even more strict match. For that, I detect the time the mic audio was first
-# louder than the silence_threshold, and start counting the reply_audio from there, first trimming off the first silent part
-# of it
+# Additionally, if the assistant has not started speaking yet, we do simple above silence_threshold interruption detection
 
 silence_threshold = 300  # same as from main
 frame_length = 512  # same as from main
@@ -50,7 +35,6 @@ class InterruptionDetection:
     interruption_check_process: Process
     interruption_check_in_queue: Queue
     interruption_check_out_queue: Queue
-    total_checks_count: int
     speaking_frame_count: int
 
     def __init__(self) -> None:
@@ -58,14 +42,9 @@ class InterruptionDetection:
         self.reply_audio_skip_frames = (
             frame_length * -28
         )  # this is the guessed delay between receiving the first buffer and ffplay starts playing it
-        self.reply_audio_skip_frames_after_first_sound = (
-            -1
-        )  # this will start counting only when first sound is heard, for feedback loops
-        self.accumulated_similarity = []
         self.audio_playback_process_pid = None
         self.interruption_check_in_queue = Queue()
         self.interruption_check_out_queue = Queue()
-        self.total_checks_count = 0
         self.speaking_frame_count = 0
 
         self.interruption_check_process = Process(
@@ -112,24 +91,7 @@ class InterruptionDetection:
         except Empty:
             pass
 
-        self.total_checks_count += 1
-
-        if self.reply_audio_skip_frames_after_first_sound == -1 and not is_silence:
-            self.reply_audio_skip_frames_after_first_sound = frame_length
-
-            # trim silent beginning from reply_audio
-            for i in range(0, len(self.reply_audio), frame_length):
-                rms = np.sqrt(
-                    np.mean(np.array(self.reply_audio[i : (i + frame_length)]) ** 2)
-                )
-                if rms < silence_threshold:
-                    self.reply_audio_skip_frames_after_first_sound += frame_length
-                else:
-                    break
-
         self.reply_audio_skip_frames += frame_length
-        if self.reply_audio_skip_frames_after_first_sound > -1:
-            self.reply_audio_skip_frames_after_first_sound += frame_length
 
         if self.reply_audio_skip_frames < 0:
             return False
@@ -138,27 +100,7 @@ class InterruptionDetection:
             self.stop()
             return False
 
-        # Do not allow interruptions on the very end of the reply audio, let it finish
-        if (
-            max(
-                self.reply_audio_skip_frames,
-                self.reply_audio_skip_frames_after_first_sound,
-            )
-            > len(self.reply_audio) - frame_length * 5
-        ):
-            return False
-
-        reply_audio_guessed_slice = self.reply_audio[
-            self.reply_audio_skip_frames : self.reply_audio_skip_frames + frame_length
-        ]
-        reply_audio_after_first_sound_slice = self.reply_audio[
-            self.reply_audio_skip_frames_after_first_sound : self.reply_audio_skip_frames_after_first_sound
-            + frame_length
-        ]
-
-        self.interruption_check_in_queue.put(
-            (pcm, reply_audio_guessed_slice, reply_audio_after_first_sound_slice)
-        )
+        self.interruption_check_in_queue.put(pcm)
         return False
 
 
@@ -167,9 +109,7 @@ def check_next_frame(in_queue: Queue, out_queue: Queue):
     batch_size = 16
     how_many_initial_batchs_to_define = 5
 
-    initial_batches_similarities = []
     initial_batches_volume = []
-    has_audio_feedback = False
     mean_volume = 0
 
     stop_counts = 0
@@ -182,29 +122,16 @@ def check_next_frame(in_queue: Queue, out_queue: Queue):
         if len(batch) < batch_size:
             continue
 
-        pcm_batch = [y for x in batch for y in x[0]]
-        prev_batch = batch
+        pcm_batch = [x for x in batch]
         batch = []
 
-        if len(initial_batches_similarities) < how_many_initial_batchs_to_define:
-            reply_batch = [y for x in prev_batch for y in x[1]]
-            if len(reply_batch) < len(pcm_batch):
-                pcm_batch = pcm_batch[: len(reply_batch)]
-            similarity = calculate_similarity(pcm_batch, reply_batch)
-            print("similarity", similarity)
-
-            initial_batches_similarities.append(similarity)
+        if len(initial_batches_volume) < how_many_initial_batchs_to_define:
             initial_batches_volume.append(calculate_volume(pcm_batch))
-            if len(initial_batches_similarities) == how_many_initial_batchs_to_define:
-                print(
-                    "np.mean(initial_batches_similarities)",
-                    np.mean(initial_batches_similarities),
+            if len(initial_batches_volume) == how_many_initial_batchs_to_define:
+                mean_volume = max(
+                    float(np.mean(initial_batches_volume)), silence_threshold
                 )
-                print("np.mean(initial_batches_volume)", np.mean(initial_batches_volume))
-                mean_volume = max(float(np.mean(initial_batches_volume)), silence_threshold)
-                if np.mean(initial_batches_similarities) > 0:
-                    batch_size = 4
-                    has_audio_feedback = True
+                batch_size = 4
 
             continue
 
@@ -217,54 +144,10 @@ def check_next_frame(in_queue: Queue, out_queue: Queue):
 
         volume_pcm = calculate_volume(pcm_batch)
 
-        if has_audio_feedback:
-            reply_batch_after_first_sound = [y for x in prev_batch for y in x[2]]
-            if len(reply_batch_after_first_sound) == 0:
-                break
-
-            # if len(reply_batch_after_first_sound) < len(pcm_batch):
-            #     pcm_batch = pcm_batch[: len(reply_batch_after_first_sound)]
-            # similarity = calculate_similarity(pcm_batch, reply_batch_after_first_sound)
-            # print("similarity", similarity)
-
-            volume_reply = calculate_volume(reply_batch_after_first_sound)
-            silence_log_diff = math.log(volume_pcm or 1) - math.log(volume_reply or 1)
-            print("silence_log_diff", silence_log_diff)
-
-            if silence_log_diff > 3.5:
-                stop_counts += 1
-                loops_since_last_stop = 0
-        else:
-            if volume_pcm >= silence_threshold:
-                stop_counts += 1
-                loops_since_last_stop = 0
-
-        # if os.getenv("DEBUG_MODE"):
-        print(
-            f"stop_counts: {stop_counts}, stop avg: {stop_counts / (loops_since_last_stop or 1)}",
-            end="\r",
-            flush=True,
-        )
+        if volume_pcm >= mean_volume:
+            stop_counts += 1
+            loops_since_last_stop = 0
 
         if stop_counts >= 2:
             out_queue.put("interrupt")
             break
-
-
-# Code from GPT-4
-def calculate_similarity(y1, y2):
-    y1 = np.array(y1)
-    y2 = np.array(y2)
-
-    # Extract MFCCs
-    mfcc1 = mfcc(y1, samplerate=16000, numcep=13)
-    mfcc2 = mfcc(y2, samplerate=16000, numcep=13)
-
-    # Calculate the mean of MFCCs
-    mean_mfcc1 = np.mean(mfcc1.T, axis=0)
-    mean_mfcc2 = np.mean(mfcc2.T, axis=0)
-
-    # Compute cosine similarity between the means
-    similarity = 1 - cosine(mean_mfcc1, mean_mfcc2)
-
-    return similarity
