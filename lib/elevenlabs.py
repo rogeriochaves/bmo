1549e4eccc33f95bb4e2c0c01df408171964cd73
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2023-05-27 14:39:15 +0200

    Manage the piping with python so that we can trigger audio started out queue event properly

--- lib/elevenlabs.py
+++ lib/elevenlabs.py
@@ -1,6 +1,7 @@
 import multiprocessing
 import os
 from queue import Empty, Queue
+import select
 import subprocess
 from threading import Thread
 from typing import Dict, Iterator, List, Union
@@ -173,59 +174,86 @@ class ElevenLabsPlayer:
 class PiperPlayer:
     min_words = 2
     piper: subprocess.Popen
+    ffplay: subprocess.Popen
+    from_piper_to_ffplay: Thread
     reply_out_queue: multiprocessing.Queue
-    first: bool
+    local_queue: Queue
+    requested_to_stop: bool
 
     def __init__(self, reply_out_queue: multiprocessing.Queue) -> None:
         self.reply_out_queue = reply_out_queue
         self.start()
 
     def start(self):
-        self.first = True
+        self.requested_to_stop = False
         self.piper = subprocess.Popen(
-            " ".join(
-                [
-                    "./piper/piper/piper",
-                    "--model",
-                    "./piper/en-us-ryan-medium.onnx",
-                    "--output_raw",
-                    "-",
-                    "|",
-                    "ffplay",
-                    "-probesize",
-                    "8192",
-                    "-f",
-                    "s16le",
-                    "-ar",
-                    "22050",
-                    "-ac",
-                    "1",
-                    "-nodisp",
-                    "-autoexit",
-                    "-",
-                ]
-            ),
-            shell=True,
+            [
+                "./piper/piper/piper",
+                "--model",
+                "./piper/en-us-ryan-medium.onnx",
+                "--output_raw",
+                "-",
+            ],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+        )
+        self.ffplay = subprocess.Popen(
+            [
+                "ffplay",
+                "-probesize",
+                "8192",
+                "-f",
+                "s16le",
+                "-ar",
+                "22050",
+                "-ac",
+                "1",
+                "-nodisp",
+                "-autoexit",
+                "-",
+            ],
             stdin=subprocess.PIPE,
             stdout=subprocess.DEVNULL,
             stderr=subprocess.STDOUT,
         )
+        self.from_piper_to_ffplay = Thread(target=self.play_as_available)
+        self.from_piper_to_ffplay.start()
 
     def request_to_stop(self):
-        self.piper.stdin.close()  # type: ignore
-        self.piper.wait()
+        self.requested_to_stop = True
+        remaining, _ = self.piper.communicate()
+        self.ffplay.stdin.write(remaining)
+        self.ffplay.stdin.close()  # type: ignore
+        self.ffplay.wait()
 
         self.reply_out_queue.put(("reply_audio_ended", None))
 
     def consume(self, word: str):
         if word == "":
             return
-        if self.first:
-            self.reply_out_queue.put(("reply_audio_started", self.piper.pid))
-            self.first = False
 
         self.piper.stdin.write((word + "\n").encode("utf-8"))  # type: ignore
-        self.piper.stdin.flush()
+        self.piper.stdin.flush()  # type: ignore
+
+    def play_as_available(self):
+        first = True
+        while not self.requested_to_stop:
+            # Wait for data to become available
+            ready_to_read, _, _ = select.select([self.piper.stdout.fileno()], [], [])
+            for stream in ready_to_read:
+                if self.requested_to_stop:
+                    return
+                if stream == self.piper.stdout.fileno():
+                    output = self.piper.stdout.read1(512 * 32)  # type: ignore
+                    if output:
+                        if first:
+                            self.reply_out_queue.put(("reply_audio_started", self.ffplay.pid))
+                            first = False
+                        self.ffplay.stdin.write(output)  # type: ignore
+                    else:
+                        # No more output, break the loop
+                        return
 
 
 def play_audio_file_non_blocking(audio_file):
