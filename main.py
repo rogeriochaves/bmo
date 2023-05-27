ec7aaf76d36788e55a83151b01d73bc5fbb3aa70
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2023-05-27 15:33:06 +0200

    Actually I dont want to keep a lot of audio before the wake word

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ porcupine = pvporcupine.create(
     keyword_paths=wakeup_keywords(),
 )
 frame_length = porcupine.frame_length  # 512
-buffer_size_when_not_listening = frame_length * 32 * 20  # keeps 20s of audio
+buffer_size_when_not_listening = frame_length * 32 * 2  # keeps 2s of audio
 buffer_size_on_active_listening = frame_length * 32 * 60  # keeps 60s of audio
 sample_rate = 16000  # sample rate for Porcupine is fixed at 16kHz
 silence_threshold = 300  # maybe need to be adjusted
